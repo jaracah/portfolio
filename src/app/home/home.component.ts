5e2e32a444d5244a0bb1432dfc50cc788289a317
Jaracah Teague <jaracahteague@gmail.com>
2024-08-29 16:29:08 -0400

    styling and hover effects

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatListModule} from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
-
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     MatGridListModule,
+    MatListModule,
     MatCardModule,
   ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit{
+  
+  cols: number = 4;
+  mainCols: number = 3;
+  flexDir: string = "column";
 
-  cols: number = 3;
+  playSound() {
+    const audio = new Audio('/hover.mp3');
+    audio.volume = 0.2;
+    audio.play();
+  }
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 
@@ -33,15 +42,21 @@ export class HomeComponent implements OnInit{
       .subscribe((state: BreakpointState) => {
         if (state.breakpoints['(max-width: 599px)']) {
           this.cols = 1;
+          this.mainCols = 1;
+          this.flexDir = "row"
         } else if (state.breakpoints['(min-width: 600px) and (max-width: 959px)']) {
           this.cols = 1;
+          this.mainCols = 1;
+          this.flexDir = "row"
         } else if (state.breakpoints['(min-width: 960px) and (max-width: 1279px)']){
-          this.cols = 2;
+          this.cols = 4;
+          this.mainCols = 3;
+          this.flexDir = "column"
         } else if (state.breakpoints['(min-width: 1280px)']){
-          this.cols = 2;
+          this.cols = 4;
+          this.mainCols = 3;
+          this.flexDir = "column"
         }
       });
-
-      console.log(this.cols);
   }
 }
